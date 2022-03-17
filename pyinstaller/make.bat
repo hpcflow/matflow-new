82e72fc332e50075f51cce26760ea7b7d247873a
aplowman <adam.jp@live.co.uk>
2022-03-17 17:53:25 +0000

    build: fix pyinstaller script

--- pyinstaller/make.bat
+++ pyinstaller/make.bat
@@ -1,3 +1,3 @@
 :: Might need to disable cloud sync. engines during this
 :: Make sure we ran `poetry install --extras=pyinstaller` (or `poetry install --no-dev --extras "pyinstaller"`)
-poetry run pyinstaller --name=hpcflow --onefile ../hpcflow/cli.py 
+poetry run pyinstaller --name=matflow --onefile ../matflow/cli.py 
