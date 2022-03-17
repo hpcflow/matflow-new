e35664b9e16c70cecdea7075e923fa0ae5c36f27
aplowman <adam.jp@live.co.uk>
2022-03-17 18:00:02 +0000

    build: update script

--- pyinstaller/make.bat
+++ pyinstaller/make.bat
@@ -1,3 +1,3 @@
 :: Might need to disable cloud sync. engines during this
-:: Make sure we ran `poetry install --extras=pyinstaller` (or `poetry install --no-dev --extras "pyinstaller"`)
+:: Make sure we ran `poetry install --extras "pyinstaller"` (or `poetry install --no-dev --extras "pyinstaller"`)
 poetry run pyinstaller --name=matflow --onefile ../matflow/cli.py 
--- pyinstaller/make.sh
+++ pyinstaller/make.sh
@@ -1,2 +1,2 @@
-# Make sure we ran `poetry install --extras=pyinstaller` (or `poetry install --no-dev --extras "pyinstaller"`)
+# Make sure we ran `poetry install --extras "pyinstaller"` (or `poetry install --no-dev --extras "pyinstaller"`)
 poetry run pyinstaller --name=matflow --onefile ../matflow/cli.py 
