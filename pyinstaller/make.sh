530859624aa9c583dc30b0d75bf9b55bbf2a768d
aplowman <adam.jp@live.co.uk>
2022-03-21 19:58:36 +0000

    build: delete pyinstaller build dirs

--- pyinstaller/make.sh
+++ pyinstaller/make.sh
@@ -1,2 +1,5 @@
 # Make sure we ran `poetry install --extras "pyinstaller"` (or `poetry install --no-dev --extras "pyinstaller"`)
+rm -r build
+rm -r dist
+rm matflow.spec
 poetry run pyinstaller --name=matflow --onefile ../matflow/cli.py 
