ebe7474ae9fb2717872fc43739947c09fbc22d4b
aplowman <adam.jp@live.co.uk>
2022-03-21 21:12:01 +0000

    refactor: store RunTimeInfo in MatFlow obj

--- matflow/__init__.py
+++ matflow/__init__.py
@@ -1,6 +1,5 @@
 import numpy as np
 from hpcflow import HPCFlowApp
-from hpcflow.runtime import RunTimeInfo
 
 from matflow._version import __version__
 
@@ -8,7 +7,6 @@ MatFlow = HPCFlowApp(
     name="matflow", version=__version__, description="Materials science workflow manager"
 )
 
-RUN_TIME_INFO = RunTimeInfo(__name__)
 
 # temporarily used just to check correct inclusion of numpy in built exes:
 a = np.random.random((10, 10))
--- matflow/api.py
+++ matflow/api.py
@@ -1,10 +1,10 @@
 import logging
 
-from hpcflow import RUN_TIME_INFO
+from matflow import MatFlow
 from hpcflow.api import *
 
 logger = logging.getLogger(__name__)
 
 
 def parameter_search():
-    logger.info(f"parameter_search; is_venv: {RUN_TIME_INFO.is_venv}")
+    logger.info(f"parameter_search; is_venv: {MatFlow.run_time_info.is_venv}")
