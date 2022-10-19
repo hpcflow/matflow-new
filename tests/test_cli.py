61efa24c176a6919e2db6a3fe6bd962aaed41e73
aplowman <adam.jp@live.co.uk>
2022-10-19 21:21:16 +0100

    test: fix

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,16 +4,16 @@ from click.testing import CliRunner
 from hpcflow import __version__ as hpcflow_version
 
 from matflow import __version__ as matflow_version
-from matflow.cli import cli
+from matflow.cli import MatFlow
 
 
 def test_version():
     runner = CliRunner()
-    result = runner.invoke(cli, args="--version")
+    result = runner.invoke(MatFlow.CLI, args="--version")
     assert result.output.strip() == f"matflow, version {matflow_version}"
 
 
 def test_hpcflow_version():
     runner = CliRunner()
-    result = runner.invoke(cli, args="--hpcflow-version")
+    result = runner.invoke(MatFlow.CLI, args="--hpcflow-version")
     assert result.output.strip() == f"hpcflow, version {hpcflow_version}"
