25b5167b3a316da74b2382ac1627fff1a934a92a
aplowman <adam.jp@live.co.uk>
2022-10-19 21:31:45 +0100

    build: update pyi scripts

--- pyinstaller/make.sh
+++ pyinstaller/make.sh
@@ -1,5 +1,7 @@
 # Make sure we ran `poetry install --extras "pyinstaller"` (or `poetry install --no-dev --extras "pyinstaller"`)
-rm -r build
-rm -r dist
-rm matflow.spec
-poetry run pyinstaller --name=matflow --onefile ../matflow/cli.py 
+# Might need to disable cloud sync. engines during this
+EXE_NAME_DEFAULT="matflow"
+LOG_LEVEL_DEFAULT="INFO"
+EXE_NAME="${1:-$EXE_NAME_DEFAULT}"
+LOG_LEVEL="${2:-$LOG_LEVEL_DEFAULT}"
+poetry run pyinstaller --log-level=$LOG_LEVEL --onefile --clean -y --name=$EXE_NAME ../matflow/cli.py
