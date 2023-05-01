d7e2423c025ddf9c0c1ebb71a04dd59a53d36268
aplowman <adam.jp@live.co.uk>
2023-05-01 14:17:23 +0100

    fix(pyi): hook datas typo

--- matflow/__pyinstaller/hook-matflow.py
+++ matflow/__pyinstaller/hook-matflow.py
@@ -2,7 +2,7 @@ from PyInstaller.utils.hooks import collect_data_files
 
 hiddenimports = ["matflow.data"]
 datas = collect_data_files("matflow.data")
-datas = collect_data_files(
+datas += collect_data_files(
     "matflow.tests",
     include_py_files=True,
     excludes=("**/__pycache__",),
