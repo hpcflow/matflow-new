23af869767d59ebea0cd392759496cd94bb539af
aplowman <adam.jp@live.co.uk>
2023-05-04 10:57:56 +0100

    fix: change default config dir so it does not conflict with old install

--- matflow/__init__.py
+++ matflow/__init__.py
@@ -1,4 +1,3 @@
-import numpy as np
 from hpcflow.sdk import App, ConfigOptions
 
 from matflow._version import __version__
@@ -6,7 +5,7 @@ from matflow.parameters import Orientations
 
 config_options = ConfigOptions(
     directory_env_var="MATFLOW_CONFIG_DIR",
-    default_directory="~/.matflow",
+    default_directory="~/.matflow-new",
     sentry_DSN="https://2463b288fd1a40f4bada9f5ff53f6811@o1180430.ingest.sentry.io/6293231",
     sentry_traces_sample_rate=1.0,
     sentry_env="main" if "a" in __version__ else "develop",
