01dff92108da38aed358990320c36e442285399f
aplowman <adam.jp@live.co.uk>
2023-11-13 20:32:42 +0000

    feat: add schema visualise_volume_element
    style: whitespace

--- matflow/data/scripts/damask/write_geom.py
+++ matflow/data/scripts/damask/write_geom.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from damask_parse.writers import write_geom as write_geom_
 
 
 def write_geom(path, volume_element):
+    path = Path(path)  # if using as a non IFG script, `path` will be a normal input
     write_geom_(dir_path=path.parent, volume_element=volume_element, name=path.name)
