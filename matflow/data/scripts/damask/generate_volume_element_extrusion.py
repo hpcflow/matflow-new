55ff67dd33c3226bc00d7dae8fc6d64db69349e7
aplowman <adam.jp@live.co.uk>
2023-11-22 14:57:56 +0000

    feat: add `modify_VE_add_buffer_zones` task schema

--- matflow/data/scripts/damask/generate_volume_element_extrusion.py
+++ matflow/data/scripts/damask/generate_volume_element_extrusion.py
@@ -9,13 +9,14 @@ def generate_volume_element_extrusion(
     phase_label,
     phase_label_mapping,
 ):
-    return {
-        "volume_element": volume_element_from_2D_microstructure(
-            microstructure_image=microstructure_image,
-            homog_label=homog_label,
-            phase_label=phase_label,
-            phase_label_mapping=phase_label_mapping,
-            depth=depth,
-            image_axes=image_axes,
-        )
-    }
+    volume_element = volume_element_from_2D_microstructure(
+        microstructure_image=microstructure_image,
+        homog_label=homog_label,
+        phase_label=phase_label,
+        phase_label_mapping=phase_label_mapping,
+        depth=depth,
+        image_axes=image_axes,
+    )
+    volume_element["grid_size"] = tuple(int(i) for i in volume_element["grid_size"])
+    volume_element["size"] = tuple(float(i) for i in volume_element["size"])
+    return {"volume_element": volume_element}
