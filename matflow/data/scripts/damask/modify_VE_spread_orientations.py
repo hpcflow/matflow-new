81e2436708a9bcc521cfda6d78eb5451576ec80b
aplowman <adam.jp@live.co.uk>
2023-12-13 19:35:07 +0000

    fix: script `modify_VE_spread_orientations` so Zarr arrays are cast to numpy

--- matflow/data/scripts/damask/modify_VE_spread_orientations.py
+++ matflow/data/scripts/damask/modify_VE_spread_orientations.py
@@ -2,7 +2,24 @@ from damask_parse.utils import spread_orientations
 
 
 def modify_VE_spread_orientations(volume_element, phases, stddev_degrees):
-    VE = spread_orientations(volume_element, phases, sigmas=stddev_degrees)
+    # need to convert Zarr arrays to numpy arrays first:
+    VE = volume_element
+    ori_copy = VE["orientations"]
+    ori_copy["quaternions"] = ori_copy["quaternions"][:]
+    VE_copy = {
+        "size": VE["size"],
+        "grid_size": VE["grid_size"][:],
+        "orientations": ori_copy,
+        "element_material_idx": VE["element_material_idx"][:],
+        "constituent_material_idx": VE["constituent_material_idx"][:],
+        "constituent_material_fraction": VE["constituent_material_fraction"][:],
+        "constituent_phase_label": VE["constituent_phase_label"][:],
+        "constituent_orientation_idx": VE["constituent_orientation_idx"][:],
+        "material_homog": VE["material_homog"][:],
+    }
+    VE = spread_orientations(
+        volume_element=VE_copy, phase_names=phases, sigmas=stddev_degrees
+    )
     VE["grid_size"] = tuple(int(i) for i in VE["grid_size"])
     VE["size"] = tuple(float(i) for i in VE["size"])
     return {"volume_element": VE}
