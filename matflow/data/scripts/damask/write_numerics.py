887ec899557014d8f2ced2ef7280b78b6e7dcd48
Michael Atkinson <mike@sprma.com>
2024-04-17 14:38:19 +0100

    fix: writing empty damask numerics file

--- matflow/data/scripts/damask/write_numerics.py
+++ matflow/data/scripts/damask/write_numerics.py
@@ -3,6 +3,11 @@ from damask_parse import write_numerics as write_numerics_
 
 
 def write_numerics(path, damask_numerics):
-    damask_numerics = {} if damask_numerics is None else damask_numerics
     path = Path(path)
+
+    if damask_numerics is None:
+        with path.open('w') as f:
+            f.write('# empty\n')
+        return
+    
     write_numerics_(dir_path=path.parent, numerics=damask_numerics, name=path.name)
