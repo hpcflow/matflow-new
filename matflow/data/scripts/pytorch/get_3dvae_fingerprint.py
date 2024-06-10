8ed28ccacdae7db3315496a8ab23db59cd7315e8
mikedwhite <mikedwhite@outlook.com>
2024-06-10 14:10:23 +0100

    feat: schema added for generating fingerprints from 3D VE

---
+++ matflow/data/scripts/pytorch/get_3dvae_fingerprint.py
@@ -0,0 +1,97 @@
+import numpy as np
+import torch
+from mftools.fingerprint.LightningVAE.LightningVAETrainer import LightningVAE_3D
+
+
+def get_3dvae_fingerprint(volume_element, checkpoint_path):
+    """Get fingerprint from PyTorch tensor of normalised Euler angles.
+
+    Parameters
+    ----------
+    volume_element : dict
+        Dream3D volume element (output from generate_volume_element schema).
+    checkpoint_path : str
+        Path to checkpoint. Should be a .ckpt file.
+
+    Returns
+    -------
+    reconstruction: torch.Tensor
+        Reconstructed RVE from the trained VAE.
+    fingerprint: torch.Tensor
+        Fingerprint from the trained VAE encoder.
+    """
+
+    # euler_angles = volume_element['DataContainers']['SyntheticVolumeDataContainer']['CellData']['EulerAngles']
+    volume_element['orientations'] = convert_ori_rep(volume_element['orientations'])
+    euler_angles = volume_element['orientations']['eulers']
+    assert not volume_element['orientations']['euler_degrees']
+    euler_angles_norm = np.zeros(euler_angles.shape)
+    euler_angles_norm[:, :, :, 0] = euler_angles[:, :, :, 0] / (2 * np.pi)
+    euler_angles_norm[:, :, :, 1] = euler_angles[:, :, :, 1] / (np.pi)
+    euler_angles_norm[:, :, :, 2] = euler_angles[:, :, :, 2] / (2 * np.pi)
+    euler_angles_norm = np.moveaxis(euler_angles_norm, 3, 0)
+    euler_angles_norm = torch.from_numpy(euler_angles_norm)
+
+    vae = LightningVAE_3D.load_from_checkpoint(checkpoint_path)
+    vae.eval()
+
+    _, _, _, fingerprint = vae(euler_angles_norm)
+
+    return {"fingerprint": torch.asarray(fingerprint)}
+
+
+def convert_ori_rep(ori_dict, type_out='euler'):
+    if type_out != 'euler' and ori_dict.get('type') != 'quat':
+        raise NotImplementedError('Can only convert euler to quat.')
+
+    assert ori_dict.get('quat_component_ordering') == 'scalar-vector'
+
+    return {
+        'type': 'euler',
+        'unit_cell_alignment': ori_dict.get('unit_cell_alignment'),
+        'euler_degrees': False,
+        'eulers': qu2eu(ori_dict['quaternions'], P=ori_dict.get('P', -1)),
+    }# orientation_coordinate_system??
+
+
+def qu2eu(qu: np.ndarray, P=1) -> np.ndarray:
+    """
+    Quaternion to Bunge Euler angles.
+
+    References
+    ----------
+    E. Bernardes and S. Viollet, PLoS ONE 17(11):e0276302, 2022
+    https://doi.org/10.1371/journal.pone.0276302
+
+    Source
+    ------
+    https://github.com/eisenforschung/DAMASK/blob/release/python/damask/_rotation.py
+
+    """
+    a =    qu[...,0:1]
+    b = -P*qu[...,3:4]
+    c = -P*qu[...,1:2]
+    d = -P*qu[...,2:3]
+
+    eu = np.block([
+        np.arctan2(b,a),
+        np.arccos(2*(a**2+b**2)/(a**2+b**2+c**2+d**2)-1),
+        np.arctan2(-d,c),
+    ])
+
+    eu_sum  = eu[...,0] + eu[...,2]
+    eu_diff = eu[...,0] - eu[...,2]
+
+    is_zero  = np.isclose(eu[...,1],0.0)
+    is_pi    = np.isclose(eu[...,1],np.pi)
+    is_ok    = ~np.logical_or(is_zero,is_pi)
+
+    eu[...,0][is_zero] =  2*eu[...,0][is_zero]
+    eu[...,0][is_pi]   = -2*eu[...,2][is_pi]
+    eu[...,2][~is_ok]  = 0.0
+    eu[...,0][is_ok]   = eu_diff[is_ok]
+    eu[...,2][is_ok]   = eu_sum [is_ok]
+
+    eu[np.logical_or(np.abs(eu)         < 1.e-6,
+                        np.abs(eu-2*np.pi) < 1.e-6)] = 0.
+    return np.where(eu < 0., eu%(np.pi*np.array([2.,1.,2.])), eu)
