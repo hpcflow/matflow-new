0c4ee27b74a22e611988c3820e7bd7872718e1f9
Adam <adam.jp@live.co.uk>
2024-09-11 21:26:43 +0100

    fix: improvments to DAMASK subset workflow

--- matflow/data/scripts/uq/evaluate_yield_stress_limit_state.py
+++ matflow/data/scripts/uq/evaluate_yield_stress_limit_state.py
@@ -1,2 +1,2 @@
-def evaluate_yield_stress_limit_state(x, threshold, yield_stress):
-    return {"g": threshold - yield_stress}
+def evaluate_yield_stress_limit_state(x, threshold_yield_stress, yield_stress):
+    return {"g": threshold_yield_stress - yield_stress}
