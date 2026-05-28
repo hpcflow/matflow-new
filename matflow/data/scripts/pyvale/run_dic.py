0dfe6d189e12a0dcee7e5d9779e1384954d29afb
Michael Atkinson <mike@sprma.com>
2026-05-28 17:24:51 +0100

    feat: Add task for running DIC with pyvale

---
+++ matflow/data/scripts/pyvale/run_dic.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from natsort import natsorted
+import pyvale.dic as dic
+import numpy as np
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = None
+
+
+def run_dic(
+    reference_image_path: str,
+    deformed_image_path: str,
+    # DIC_ROI,
+    **kwargs
+):
+    reference_image_path = Path(reference_image_path)
+    ref_image = np.array(Image.open(reference_image_path))
+
+    deformed_image_path = Path(deformed_image_path)
+    deformed_image_paths = natsorted(
+        deformed_image_path.parent.glob(deformed_image_path.name)
+    )
+    def_images = []
+    for deformed_image_path in deformed_image_paths:
+        if deformed_image_path == reference_image_path:
+            continue
+        def_images.append(np.array(Image.open(deformed_image_path)))
+    def_images = np.array(def_images)
+
+    roi = dic.RegionOfInterest(ref_image=ref_image)
+    roi.rect_boundary(left=0, right=0, top=0, bottom=0)
+    roi.seed = [1000, 1000]
+
+    dic_args = {k: v for k, v in kwargs.items() if v is not None}
+    dic.calculate_2d(
+        reference=ref_image,
+        deformed=def_images,
+        roi_mask=roi.mask,
+        seed=roi.seed,
+        output_at_end=True,
+        output_binary=True,
+        output_below_threshold=True,
+        **dic_args
+    )
