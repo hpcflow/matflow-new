412cffdfe3c8b29c17519f93acc1bc6e84ea8292
hpcflow-actions <hpcflow-actions@users.noreply.github.com>
2026-07-06 12:39:19 +0000

    pre-commit fixes [skip ci]

--- matflow/data/scripts/damask/write_geom_from_dream_3D.py
+++ matflow/data/scripts/damask/write_geom_from_dream_3D.py
@@ -1,9 +1,10 @@
 from damask import GeomGrid
 import pathlib
 
+
 def write_geom_from_dream_3D(dream_3D_hdf5_file_path: str) -> dict:
     """Write DAMASK geometry file from .dream3d file"""
     grid = GeomGrid.load_DREAM3D(dream_3D_hdf5_file_path, feature_IDs="FeatureIds")
-    grid.save('geom.vti')
+    grid.save("geom.vti")
     geom_path = str(pathlib.Path().resolve() / "geom.vti")
     return {"geom_path": geom_path}
