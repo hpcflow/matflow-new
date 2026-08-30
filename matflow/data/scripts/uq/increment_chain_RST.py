fix: improve numerical stability in SuS-DA, and note that temperature should have an appropriate scale

matflow/data/scripts/uq/increment_chain_RST.py
@@ -1,11 +1,14 @@
 import os
 
 import numpy as np
+from scipy.special import log_expit
 
 
-def coarse_weight(g_coarse, threshold, temperature):
+def log_surrogate_weight(g_coarse, threshold, temperature):
+    """Note temperature should be of a similar order of magnitude to `g_coarse` and
+    `threshold`."""
     z = (g_coarse - threshold) / temperature
-    return 1.0 / (1.0 + np.exp(-z))
+    return log_expit(z)
 
 
 def increment_chain_RST(
@@ -89,13 +92,13 @@ def increment_chain_RST(
             # min(1, s(current_x) / s(psi))
             # ------------------------------------------------
 
-            s_current = coarse_weight(current_gc, threshold, temperature)
-            s_psi = coarse_weight(gc, threshold, temperature)
+            log_s_current = log_surrogate_weight(current_gc, threshold, temperature)
+            log_s_psi = log_surrogate_weight(gc, threshold, temperature)
 
-            alpha_fine = min(1.0, s_current / s_psi)
+            log_alpha_fine = min(0.0, log_s_current - log_s_psi)
 
             random_num = rng.random()
-            fine_accept = random_num < alpha_fine
+            fine_accept = np.log(random_num) < log_alpha_fine
 
             if fine_accept:
                 new_x = x[:]  # convert to Numpy array

matflow/data/scripts/uq/increment_sub_chain.py
@@ -1,11 +1,14 @@
 import os
 
 import numpy as np
+from scipy.special import log_expit
 
 
-def coarse_weight(g_coarse, threshold, temperature):
+def log_surrogate_weight(g_coarse, threshold, temperature):
+    """Note temperature should be of a similar order of magnitude to `g_coarse` and
+    `threshold`."""
     z = (g_coarse - threshold) / temperature
-    return 1.0 / (1.0 + np.exp(-z))
+    return log_expit(z)
 
 
 def increment_sub_chain(
@@ -77,12 +80,12 @@ def increment_sub_chain(
 
     else:
 
-        s_current = coarse_weight(current_sub_chain_gc, threshold, temperature)
-        s_trial = coarse_weight(trial_gc, threshold, temperature)
-        alpha = min(1.0, s_trial / s_current)
+        log_s_current = log_surrogate_weight(current_sub_chain_gc, threshold, temperature)
+        log_s_trial = log_surrogate_weight(trial_gc, threshold, temperature)
+        log_alpha = min(0.0, log_s_trial - log_s_current)
 
         random_num = rng.random()
-        is_accept = random_num < alpha
+        is_accept = np.log(random_num) < log_alpha
 
         new_x = trial_x if is_accept else current_sub_chain_x
         new_gc = trial_gc if is_accept else current_sub_chain_gc